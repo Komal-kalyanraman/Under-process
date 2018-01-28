b18aeb8eefc24bf1c324d6245c3ae1797514e724
Komal-kalyanraman <komal.iitkanpur@gmail.com>
2018-01-28 12:37:21 +0530

    Master

---
+++ SPI AVR Arduino/Arduino-master/Arduino-master.ino
@@ -0,0 +1,28 @@
+#include <SPI.h>
+
+const int chipSelectPin = 10;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(chipSelectPin, OUTPUT);
+  digitalWrite(chipSelectPin, HIGH);
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+  SPI.begin();
+}
+
+void send(unsigned int n) {
+  Serial.print("Sending ");
+  Serial.print(n);
+//  digitalWrite(chipSelectPin, LOW);
+  unsigned int response = SPI.transfer(n);
+//  digitalWrite(chipSelectPin, HIGH);
+  Serial.print(" .. received ");
+  Serial.println(response);
+  delay(100);
+}
+
+void loop() {
+    for (int i=0; i<127; i++) {
+      send(i);
+    }
+}
